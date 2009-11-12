29ff2f6f55acc165b49838c3fa1eb7301570fe21
Luke Dupin <ldupin@CRF450r.(none)>
2009-11-11 17:14:14 -0800

    Dude, version trails

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION
-  #define VERSION      "0.0.4"
-  #define VERSION_DATE "Mon Nov 09 00:52:05 -0800 2009"
-  #define VERSION_TAG  "version_0_0_4"
+  #define VERSION      "0.0.5"
+  #define VERSION_DATE "Wed Nov 11 03:23:27 -0800 2009"
+  #define VERSION_TAG  "version_0_0_5"
 #endif
